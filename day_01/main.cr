2447d7f277cf9b013dba09b5deb9db074faccd62
Richard Nienaber <rjnienaber@gmail.com>
2022-12-03 00:30:01 +0000

    update with .each_line.map

--- day_01/main.cr
+++ day_01/main.cr
@@ -1,11 +1,3 @@
-totals = [] of Int32
-current = 0
-ARGF.each_line do |line|
-  next current += line.strip.to_i unless line == ""
-  totals << current
-  current = 0
-end
-
-sorted = totals.sort
+sorted = ARGF.each_line.map(&.to_i?).chunk_while { |v| v }.map(&.compact.sum).to_a.sort
 puts "top elf: #{sorted[-1]}"           # 70613
 puts "top 3 total: #{sorted[-3..].sum}" # 205805
--- day_01/main.golf.cr
+++ day_01/main.golf.cr
@@ -1,1 +1,1 @@
-t = ARGF.gets_to_end.split("\n\n").map(&.split.map(&.to_i)).map(&.sum).sort; p t[-1]; p t[-3..].sum
+t=ARGF.each_line.map(&.to_i?).chunk_while{|v|v}.map(&.compact.sum).to_a.sort;p t[-1];p t[-3..].sum
